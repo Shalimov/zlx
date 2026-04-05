6a149541b31e31405684516ed755eed9f6f01a4f
Igor Shalimov <igor.shalimov@poqcommerce.com>
2026-04-05 20:55:32 +0200

    feat: use array list as a stack impl for the vm and use fixed buffer alloc for the stack

--- src/main.zig
+++ src/main.zig
@@ -4,23 +4,29 @@ const OpCode = @import("op_code.zig").OpCode;
 const debug = @import("debug.zig");
 const VirtualMachine = @import("vm.zig").VirtualMachine;
 
+const VM_MAX_STACK_SIZE = 1024;
+var stack_buffer: [VM_MAX_STACK_SIZE]u8 = undefined;
+
 pub fn main(init: std.process.Init) !void {
-    const alloc = init.gpa;
+    const gpa = init.gpa;
+    var FixedBufferAllocator = std.heap.FixedBufferAllocator.init(&stack_buffer);
+    const fba = FixedBufferAllocator.allocator();
 
-    var first_chunk: Chunk = .init();
-    defer first_chunk.deinit(alloc);
+    var chunk: Chunk = .init();
+    defer chunk.deinit(gpa);
 
-    var vm: VirtualMachine = try VirtualMachine.init(alloc);
-    defer vm.deinit(alloc);
+    var vm: VirtualMachine = .init;
+    defer vm.deinit(fba);
 
-    try first_chunk.writeConstant(alloc, 120, 123);
-    try first_chunk.writeConstant(alloc, 110, 123);
-    try first_chunk.write(alloc, @intFromEnum(OpCode.op_sub), 123);
-    try first_chunk.writeConstant(alloc, 2, 123);
-    try first_chunk.write(alloc, @intFromEnum(OpCode.op_div), 123);
+    try chunk.writeConstant(gpa, 120, 123);
+    try chunk.writeConstant(gpa, 110, 123);
+    try chunk.write(gpa, @intFromEnum(OpCode.op_sub), 123);
+    try chunk.writeConstant(gpa, 2, 123);
+    try chunk.write(gpa, @intFromEnum(OpCode.op_div), 123);
 
-    try first_chunk.write(alloc, @intFromEnum(OpCode.op_negate), 123);
-    try first_chunk.write(alloc, @intFromEnum(OpCode.op_return), 123);
+    try chunk.write(gpa, @intFromEnum(OpCode.op_negate), 123);
 
-    _ = vm.interpret(&first_chunk);
+    try chunk.write(gpa, @intFromEnum(OpCode.op_return), 123);
+
+    _ = try vm.interpret(fba, &chunk);
 }
--- src/vm.zig
+++ src/vm.zig
@@ -5,77 +5,49 @@ const Chunk = @import("chunk.zig").Chunk;
 const val = @import("value.zig");
 const debug = @import("debug.zig");
 
-pub const InstructionResult = enum { ok, compile_error, runtime_error };
-
-const Stack = struct {
-    stack: []val.Value,
-    stack_top: [*]val.Value,
-
-    fn deinit(self: *Stack, alloc: std.mem.Allocator) void {
-        alloc.free(self.stack);
-    }
-
-    fn pop(self: *Stack) val.Value {
-        self.stack_top -= 1;
-
-        return @as(val.Value, self.stack_top[0]);
-    }
-
-    fn push(self: *Stack, value: val.Value) void {
-        self.stack_top[0] = value;
-        self.stack_top += 1;
-    }
-
-    fn reset(self: *Stack) void {
-        self.stack_top = self.stack.ptr;
-    }
-
-    inline fn debugPrint(self: Stack) void {
-        std.debug.print("        ", .{});
-
-        var slot = self.stack.ptr;
-        while (@intFromPtr(slot) < @intFromPtr(self.stack_top)) : (slot += 1) {
-            std.debug.print("[", .{});
-            val.printValue(slot[0]);
-            std.debug.print("]", .{});
-        }
-
-        std.debug.print("\n", .{});
-    }
+pub const InstructionResult = enum {
+    ok,
 };
 
-pub const VirtualMachine = struct {
-    const MAX_STACK = 256;
+const InterpretError = (std.mem.Allocator.Error || error{
+    CompileError,
+    RuntimeError,
+});
 
+pub const VirtualMachine = struct {
     ip: [*]u8,
     chunk: *Chunk,
-    stack: Stack,
+    stack: std.ArrayList(val.Value),
 
-    pub fn init(alloc: std.mem.Allocator) !VirtualMachine {
-        const stack = try alloc.alloc(val.Value, MAX_STACK);
+    pub const init: @This() = .{
+        .chunk = undefined,
+        .ip = undefined,
+        .stack = .empty,
+    };
 
-        return VirtualMachine{
-            .chunk = undefined,
-            .ip = undefined,
-            .stack = .{ .stack = stack, .stack_top = stack.ptr },
-        };
-    }
-
-    pub fn interpret(self: *VirtualMachine, chunk: *Chunk) InstructionResult {
+    pub fn interpret(self: *VirtualMachine, alloc: std.mem.Allocator, chunk: *Chunk) InterpretError!InstructionResult {
         self.chunk = chunk;
         self.ip = chunk.code.items.ptr;
 
-        return self.run();
+        return try self.run(alloc);
     }
 
     pub fn deinit(self: *VirtualMachine, alloc: std.mem.Allocator) void {
         self.stack.deinit(alloc);
     }
 
-    fn run(self: *VirtualMachine) InstructionResult {
+    fn run(self: *VirtualMachine, alloc: std.mem.Allocator) InterpretError!InstructionResult {
         while (true) {
-            if (comptime builtin.mode == .Debug) {
-                self.stack.debugPrint();
+            if (builtin.mode == .Debug) {
+                std.debug.print("        ", .{});
+
+                for (self.stack.items) |item| {
+                    std.debug.print("[", .{});
+                    val.printValue(item);
+                    std.debug.print("]", .{});
+                }
+
+                std.debug.print("\n", .{});
                 _ = debug.disassembleInstruction(self.chunk, self.ip - self.chunk.code.items.ptr);
             }
 
@@ -84,7 +56,7 @@ pub const VirtualMachine = struct {
             switch (op_code) {
                 .op_constant => {
                     const value = self.chunk.values.items[self.advance()];
-                    self.stack.push(value);
+                    try self.stack.append(alloc, value);
                 },
                 .op_constant_long => {
                     const low_part: u8 = self.advance();
@@ -93,16 +65,17 @@ pub const VirtualMachine = struct {
                     const_index = (const_index << 8) | low_part;
 
                     const value = self.chunk.values.items[const_index];
-                    self.stack.push(value);
+                    try self.stack.append(alloc, value);
                 },
                 .op_negate => {
-                    self.stack.push(-self.stack.pop());
+                    const top = self.stack.items.len - 1;
+                    self.stack.items[top] = -self.stack.items[top];
                 },
                 inline .op_add, .op_sub, .op_mul, .op_div => |op| {
-                    const x2 = self.stack.pop();
-                    const x1 = self.stack.pop();
+                    const x2 = self.stack.pop().?;
+                    const x1 = self.stack.pop().?;
 
-                    self.stack.push(switch (op) {
+                    try self.stack.append(alloc, switch (op) {
                         .op_add => x1 + x2,
                         .op_sub => x1 - x2,
                         .op_mul => x1 * x2,
@@ -111,7 +84,7 @@ pub const VirtualMachine = struct {
                     });
                 },
                 .op_return => {
-                    val.printValue(self.stack.pop());
+                    val.printValue(self.stack.pop().?);
                     std.debug.print("\n", .{});
 
                     return InstructionResult.ok;
